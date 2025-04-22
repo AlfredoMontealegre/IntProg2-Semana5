5dd5e37d3271de0fb934d50d1d5829160c6899a7
AlfredoMontealegre <montealfredo24@gmail.com>
2025-04-22 07:48:05 -0600

    Se realizo un formato a la Salida

--- actividad3/ejercicio1.py
+++ actividad3/ejercicio1.py
@@ -5,11 +5,13 @@ calificacion3 = int(input("Ingrese la calificacion 3: "))
 suma = calificacion1+calificacion2+calificacion3
 promedio = suma/3
 
+#El ":>3" hace que se ordene dependiendo la longitud y valores, como asi 1 unidad, 2 decena, 3 centena. etc.
+
 print("="*20)
-print(f"""Calificacion 1: {calificacion1}
-Calificacion 2: {calificacion2}
-Calificacion 3: {calificacion3}
-Promedio: {promedio:.0f} """)
+print(f"""Calificacion 1: {calificacion1:>3}
+Calificacion 2: {calificacion2:>3}
+Calificacion 3: {calificacion3:>3} 
+{"Promedio:":<15} {promedio:>3.0f} """)
 print("="*20)
 
 # 0f = Sin Decimales (El 0 es la cantidad supongo de decimales mostrados)
