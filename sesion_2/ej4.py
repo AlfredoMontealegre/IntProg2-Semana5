91688002e6dbf9b2067a10e86b52e5b7e821bd2c
AlfredoMontealegre <montealfredo24@gmail.com>
2025-04-25 08:37:23 -0600

    Triangulo

---
+++ sesion_2/ej4.py
@@ -0,0 +1,18 @@
+# Crea un programa que tome 3 numero y cree un triangulo
+
+def es_triangulo(lado1,lado2,lado3):
+    suma = lado1 + lado2
+    if (suma > lado3):
+        return "Es un triangulo"
+    else:
+        return "No es un triangulo"
+    
+def main():
+    print("Ingresa los siguientes valores")
+    print("/"*30)
+    lado1 = float(input("Lado 1: "))
+    lado2 = float(input("Lado 2: "))
+    lado3 = float(input("Lado 3: "))
+    print(es_triangulo(lado1, lado2, lado3))
+
+    main()
